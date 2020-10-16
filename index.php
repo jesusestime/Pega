3fe73fc8b316afef59b7151e5787f1d205c38f2e
Pegasus03 <65563091+Estime257@users.noreply.github.com>
2020-10-16 14:36:21 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+TP1
+
+
+
+<?php 
+$nom = 'marc';
+$nom1 = 'doe';
+$note1=10;
+$note2=20;
+echo "Bonjour $nom $nom1 vous avez eu"." ".(($note1+$note2)/2)." de moyenne";
+?>
---
+++ r.php
@@ -0,0 +1,5 @@
+<?php
+include ('fonction users.php');
+echo "hello lorem ipsum fgjhvhfuvhufbvfubu";
+$ret=reponse();
+var_dump($ret);
